77dad16bb01b82012a8d24e9d6ae2dce4981dc3f
Alyson <80166983+Alyson02@users.noreply.github.com>
2022-04-16 19:57:53 -0300

    CRUD de Pessoa
    implementa rotas de GetById, Update, Create e Delete restantes

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -1,6 +1,7 @@
 const database = require('../models');
 
 class PessoaController{
+
     static async GetAll(req, res){
         try {
             const pessoas = await database.Pessoas.findAll();
@@ -9,6 +10,89 @@ class PessoaController{
             return res.status(500).json(error.message);    
         }
     }
+
+    static async Get(req, res){
+        const { idPessoa } = req.params;
+
+        try {
+            const pessoa = await database.Pessoas.findOne({
+                where: {
+                    id: idPessoa
+                }
+            })
+
+            if(pessoa == null)
+                throw new Error("Pessoa não encontrada")
+
+            return res.status(200).json(pessoa);
+        } catch (error) {
+            return res.status(404).json({ mensagem: error.message  });
+        }
+    }
+
+    static async Create(req, res){
+        const _pessoa = req.body; 
+
+        try {
+            const pessoa = await database.Pessoas.create(_pessoa);
+            res.status(201).json(pessoa);
+        } catch (error) {
+            return res.status(500).json(error.message); 
+        }
+    }
+
+    static async Update(req, res){
+        const {idPessoa} = req.params;
+        const _pessoa = req.body;
+        
+        try {
+            await database.Pessoas.update(_pessoa, {
+                where: { id: idPessoa }
+            });
+
+            const pessoa = await database.Pessoas.findOne({
+                where: {
+                    id: idPessoa
+                }
+            });  
+
+            if(pessoa == null)
+                throw new Error('Pessoa nao encontrada');
+            
+            return res.status(204).end();    
+        } catch (error) {
+            return res.status(404).json({ mensagem: error.message });
+        }
+    }
+
+    static async Delete(req, res){
+        const {idPessoa} = req.params
+
+        try {
+            const pessoa = await database.Pessoas.findOne({
+                where: {
+                    id: idPessoa
+                }
+            });
+
+            const resultado = await database.Pessoas.destroy({
+                where: { id: idPessoa }
+            }); 
+
+            if(pessoa == null)
+                throw new Error('Pessoa nao encontrada');
+
+            console.log(resultado);
+
+            if(resultado == 0)
+                throw new Error('Não foi possivel excluir a pessoa ' + idPessoa);
+
+            return res.status(204).end(); 
+        } catch (error) { 
+            return res.status(500).json({ mensagem : error.message }); 
+        }
+    }
+
 }
 
 module.exports = PessoaController;
--- api/routes/pessoaRoute.js
+++ api/routes/pessoaRoute.js
@@ -1,6 +1,14 @@
 const  router = require('express').Router();
 const PessoaController = require('../controllers/PessoaController');
 
-router.get('/pessoas', PessoaController.GetAll)
+router.get('/pessoas', PessoaController.GetAll);
+
+router.get('/pessoas/:idPessoa', PessoaController.Get);
+
+router.post('/pessoas', PessoaController.Create);
+
+router.put('/pessoas/:idPessoa', PessoaController.Update);
+
+router.delete('/pessoas/:idPessoa', PessoaController.Delete)
 
 module.exports = router;
