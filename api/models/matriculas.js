b9d673b20764928c3cc88bf638b8938537e14a80
Alyson <80166983+Alyson02@users.noreply.github.com>
2022-04-17 00:17:55 -0300

    Completa Banco de Dados
    Implementa tabelas restantes fazendo seu relacionamento usando o ORM
    sequelize e popula as tabelas através de seeders

---
+++ api/models/matriculas.js
@@ -0,0 +1,24 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Matriculas extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Matriculas.belongsTo(models.Pessoas);
+      Matriculas.belongsTo(models.Turmas);
+    }
+  }
+  Matriculas.init({
+    status: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Matriculas',
+  });
+  return Matriculas;
+};
---
+++ api/models/turmas.js
@@ -0,0 +1,22 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Turmas extends Model {
+    static associate(models) {
+      Turmas.hasMany(models.Matriculas, {
+        foreignKey: 'turma_id'
+      });
+      Turmas.belongsTo(models.Pessoas);
+      Turmas.belongsTo(models.Niveis);
+    }
+  }
+  Turmas.init({
+    data_inicio: DataTypes.DATEONLY
+  }, {
+    sequelize,
+    modelName: 'Turmas',
+  });
+  return Turmas;
+};
